6a39875bdc872cc9bbaeea8d30322e545d730906
swamirakesh <rswami5579@gmail.com>
2024-12-07 13:04:27 +0530

    Added new file

---
+++ casting.java
@@ -0,0 +1,12 @@
+public class casting
+{
+    public static void main(String args[])
+    {
+        int myBikeNum=5579;
+        double myWeight=55.8;
+        double myBikeNumInDecimal=myBikeNum;
+        System.out.println(myBikeNumInDecimal);
+        int myWeightInInt=(int)myWeight;
+        System.out.println(myWeightInInt);
+    }
+}
